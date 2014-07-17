aaffa84a3c48336d817ae0896726038e78b2282f
Vitor Peixoto <vitor@4ponto2.com.br>
2014-07-17 10:58:54 -0300

    primeiro
    commit

---
+++ Assets/Scripts/RayScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayScript : MonoBehaviour {
+
+	Ray ray;
+	RaycastHit2D hit;
+	public GameObject obj;
+	private Animator animator;
+	public BossScript bossscript;
+
+	void Start(){
+		animator = obj.GetComponent<Animator>();
+	}
+
+	void Update(){
+
+		if(Input.GetKey(KeyCode.Mouse0)){
+			print("teste");;
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+			//hit = Physics2D.Raycast(cameraPosition, mousePosition, distance (optional));
+			
+			if(hit.collider != null){
+				print("teste2");;
+
+				if(hit.collider.gameObject.tag=="Player"){
+					print("teste3");;
+					animator.SetTrigger("Hit");
+				}
+				//Debug.DrawRay(ray.origin, hit.point, Color.red);
+				//Debug.Log("object clicked: "+hit.collider.tag);
+
+				//Destroy(GameObject.Find(hit.collider.gameObject.name));
+			}
+		}
+	}
+}
